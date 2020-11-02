b63ce56831a5daeca35a6fc914b432e05c28d13f
Ajay Ajith <drax0x01.dev@google.com>
2020-11-02 12:03:51 +0530

    testing

---
+++ Go_Examples/Basics/datastructures.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a = make([5] int)
+}
---
+++ Go_Examples/Basics/if_else.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a = 5
+	if(a%2 == 0){
+		fmt.Println("is even")
+	}else{
+		fmt.Println("is odd")
+	}
+}
