caf82461010eac85edfe8437f769d399ef933daa
Drax <drax0x01.dev@gmail.com>
2021-02-20 23:08:13 +0530

    modularized the code

--- Basics/HelloWorld.go
+++
@@ -1,7 +0,0 @@
-package basics
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hello World!")
-}
---
+++ HelloWorld/HelloWorld.go
@@ -0,0 +1,11 @@
+package helloworld
+
+/*
+A go program requires a package name at the top
+*/
+import "fmt"
+
+//a function which prints helloworld
+func main() {
+	fmt.Println("Hello World!")
+}
