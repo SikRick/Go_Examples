fb0ecaaf65c6af6eb4eee25b3b4135690a26ab3b
Drax <drax0x01.dev@gmail.com>
2021-02-20 23:15:17 +0530

    added a channels example

--- HelloWorld/HelloWorld.go
+++ HelloWorld/HelloWorld.go
@@ -5,7 +5,7 @@ A go program requires a package name at the top
 */
 import "fmt"
 
-//a function which prints helloworld
+//a function which prints a helloworld text
 func main() {
 	fmt.Println("Hello World!")
 }
